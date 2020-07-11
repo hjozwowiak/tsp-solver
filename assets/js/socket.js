19ff8735c9480568c85204f83e55d9c9be470077
Hubert Józwowiak <hubert.jozwowiak@appunite.com>
2020-07-11 19:34:35 +0200

    added working solution for greedy algorithm

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -18,9 +18,10 @@ chatInput.addEventListener("keypress", (event) => {
 });
 
 channel.on("solution", (payload) => {
-    let messageItem = document.createElement("p");
-    messageItem.innerText = `[${Date()}] ${payload.body}`;
-    messagesContainer.appendChild(messageItem);
+    console.log(payload);
+    // let messageItem = document.createElement("p");
+    // messageItem.innerText = `[${Date()}] ${payload.body}`;
+    // messagesContainer.appendChild(messageItem);
 });
 
 channel
@@ -33,21 +34,24 @@ channel
     });
 
 const solveGreedy = () => {
-    channel.push("solve", {body: {
-        starting_point: {
-            index: null,
-            coords: [12, 34],
-        },
-        list_of_points: [
+    channel.push("solve", {
+        body: [
             { index: 0, coords: [96, 31] },
             { index: 1, coords: [51, 48] },
             { index: 2, coords: [46, 20] },
+            { index: 3, coords: [88, 83] },
+            { index: 4, coords: [42, 98] },
+            { index: 5, coords: [35, 91] },
+            { index: 6, coords: [12, 78] },
+            { index: 7, coords: [34, 40] },
+            { index: 8, coords: [6, 12] },
+            { index: 9, coords: [94, 89] },
         ],
-    }});
+    });
 };
 
-document.querySelector('#solveGreedy').addEventListener("click", () => {
+document.querySelector("#solveGreedy").addEventListener("click", () => {
     solveGreedy();
-})
+});
 
 export default socket;
--- lib/tsp_solver/solver/solver.ex
+++ lib/tsp_solver/solver/solver.ex
@@ -1,63 +1,13 @@
+alias TspSolver.Solver.Helper
+alias TspSolver.Solver.Greedy
+
 defmodule TspSolver.Solver do
-  @spec solve_greedy(map() | list(), list()) :: list()
+  @spec solve_traveling_salesman_problem(binary(), list()) :: {:ok, list()}
+  def solve_traveling_salesman_problem("greedy", list_of_all_points) do
+    {:ok, %{starting_point: starting_point, list_of_points: list_of_points}} =
+      Helper.prepare_data_to_process(list_of_all_points)
 
-  def solve_greedy(point_from = %{}, list_of_points) do
-    solve_greedy([point_from], list_of_points)
-  end
-
-  def solve_greedy(list_of_points_from, []) do
-    list_of_points_from
-  end
-
-  def solve_greedy([point_from | _] = list_of_points_from, list_of_points_to) do
-    closest_point = find_closest_point(point_from, list_of_points_to)
-
-    remaining_points =
-      Enum.filter(list_of_points_to, fn point ->
-        point != closest_point
-      end)
-
-    {[closest_point | list_of_points_from], remaining_points}
-
-    solve_greedy([closest_point | list_of_points_from], remaining_points)
-  end
-
-  @spec find_closest_point(
-          point_from :: %{index: number() | nil, coords: {binary(), binary()}},
-          list()
-        ) :: %{index: number() | nil, coords: {binary(), binary()}}
-  defp find_closest_point(point_from, list_of_points) do
-    %{index: closest_point_index} =
-      get_distance_for_list_of_points(point_from, list_of_points)
-      |> find_smallest_distance()
-
-    Enum.find(list_of_points, fn %{index: point_index} ->
-      point_index == closest_point_index
-    end)
-  end
-
-  @spec get_distance_for_list_of_points(
-          %{index: number() | nil, coords: point_from_coords :: {binary(), binary()}},
-          list()
-        ) :: list()
-  defp get_distance_for_list_of_points(%{coords: point_from_coords}, list_of_points) do
-    Enum.map(list_of_points, fn point ->
-      %{index: point_index, coords: point_to_coords} = point
-      %{index: point_index, distance_to: distance_between(point_from_coords, point_to_coords)}
-    end)
-  end
-
-  @spec distance_between({x1 :: number(), y1 :: number()}, {x2 :: number(), y2 :: number()}) ::
-          number()
-  defp distance_between({x1, y1}, {x2, y2}) do
-    (:math.pow(abs(x1 - x2), 2) + :math.pow(abs(y1 - y2), 2))
-    |> :math.sqrt()
-  end
-
-  @spec find_smallest_distance(list_of_distances :: list()) :: %{index: number()}
-  defp find_smallest_distance(list_of_distances) do
-    Enum.min_by(list_of_distances, fn %{distance_to: distance} ->
-      distance
-    end)
+    Greedy.solve_greedy(starting_point, list_of_points)
+    |> Helper.prepare_data_to_send()
   end
 end
