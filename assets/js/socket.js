53600d1a0bdc9c7852a84c0f8eaadb2deb6279b0
Hubert Józwowiak <hubert.jozwowiak@gmail.com>
2021-10-10 21:10:16 +0200

    [feat]: refactor data visualization

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -9,7 +9,7 @@ const SOLUTION_CANVAS_CONTEXT = document
   .querySelector('#resultCanvas')
   .getContext('2d')
 const SOLUTION_CANVAS_SIZE = 500
-const SOLUTION_CANVAS_SCALE = 5
+let SOLUTION_CANVAS_SCALE = 5
 let INSTANCE = null
 
 SOLUTION_CANVAS_CONTEXT.canvas.width = SOLUTION_CANVAS_SIZE
@@ -17,42 +17,13 @@ SOLUTION_CANVAS_CONTEXT.canvas.height = SOLUTION_CANVAS_SIZE
 
 const CHANNEL = socket.channel(`solver:${ID}`, {})
 
-CHANNEL
-  .join()
+CHANNEL.join()
   .receive('ok', (resp) => {
     console.log('Joined successfully', resp)
   })
   .receive('error', (resp) => {
     console.log('Unable to join', resp)
   })
-
-const generateInstance = () => {
-  INSTANCE = [
-    { index: 0, coords: [96, 31] },
-    { index: 1, coords: [51, 48] },
-    { index: 2, coords: [94, 50] },
-    { index: 3, coords: [88, 83] },
-    { index: 4, coords: [42, 98] },
-    { index: 5, coords: [35, 91] },
-    { index: 6, coords: [12, 78] },
-    { index: 7, coords: [34, 40] },
-    { index: 8, coords: [6, 12] },
-    { index: 9, coords: [94, 89] },
-    { index: 10, coords: [24, 6] },
-    { index: 11, coords: [41, 31] },
-    { index: 12, coords: [87, 20] },
-    { index: 13, coords: [79, 92] },
-    { index: 14, coords: [76, 23] },
-    { index: 15, coords: [65, 87] },
-    { index: 16, coords: [34, 55] },
-    { index: 17, coords: [14, 82] },
-    { index: 18, coords: [6, 59] },
-    { index: 19, coords: [65, 7] },
-  ]
-
-  const parsedPoints = parseCoordsData(INSTANCE)
-  drawPoints(SOLUTION_CANVAS_CONTEXT, parsedPoints[1])
-}
 
 const solveGreedy = () => {
   CHANNEL.push('solve', {
@@ -81,11 +52,26 @@ const parseCoordsData = (data) => {
 const drawSinglePoint = (ctx, [x, y], pointType) => {
   if (pointType === 'big') {
     ctx.fillStyle = '#f00'
-    ctx.fillRect(x * SOLUTION_CANVAS_SCALE - 2, y * SOLUTION_CANVAS_SCALE - 1, 5, 3)
-    ctx.fillRect(x * SOLUTION_CANVAS_SCALE - 1, y * SOLUTION_CANVAS_SCALE - 2, 3, 5)
+    ctx.fillRect(
+      x * SOLUTION_CANVAS_SCALE - 2,
+      y * SOLUTION_CANVAS_SCALE - 1,
+      5,
+      3
+    )
+    ctx.fillRect(
+      x * SOLUTION_CANVAS_SCALE - 1,
+      y * SOLUTION_CANVAS_SCALE - 2,
+      3,
+      5
+    )
   } else {
     ctx.fillStyle = '#000'
-    ctx.fillRect(x * SOLUTION_CANVAS_SCALE - 1, y * SOLUTION_CANVAS_SCALE - 1, 3, 3)
+    ctx.fillRect(
+      x * SOLUTION_CANVAS_SCALE - 1,
+      y * SOLUTION_CANVAS_SCALE - 1,
+      3,
+      3
+    )
   }
 }
 
@@ -106,6 +92,14 @@ const drawLine = (ctx, [x1, y1], [x2, y2]) => {
 }
 
 const drawPath = (ctx, listOfPoints) => {
+  let timeout = null
+
+  if (listOfPoints.length <= 30) {
+    timeout = 200
+  } else {
+    timeout = 6000 / listOfPoints.length
+  }
+
   for (let i = 1; i <= listOfPoints.length; i++) {
     setTimeout(() => {
       if (i !== listOfPoints.length) {
@@ -113,8 +107,42 @@ const drawPath = (ctx, listOfPoints) => {
       } else {
         drawLine(ctx, listOfPoints[listOfPoints.length - 1], listOfPoints[0])
       }
-    }, i * 250)
+    }, i * timeout)
   }
+}
+
+const clearCanvas = (ctx, size) => {
+  ctx.fillStyle = '#fff'
+  ctx.fillRect(0, 0, size, size)
+}
+
+const getInstance = (min, max, count) => {
+  let allPoints = [],
+    selectedPoints = []
+
+  for (let x = min; x <= max; x++) {
+    for (let y = min; y <= max; y++) {
+      allPoints.push([x, y])
+    }
+  }
+
+  for (let n = 0; n < count; n++) {
+    const randomIndex = Math.round(Math.random() * (allPoints.length - 1))
+    selectedPoints.push({ index: n, coords: allPoints[randomIndex] })
+    allPoints.splice(randomIndex, 1)
+  }
+
+  return selectedPoints
+}
+
+const generateInstance = (min, max, count) => {
+  clearCanvas(SOLUTION_CANVAS_CONTEXT, SOLUTION_CANVAS_SIZE)
+  SOLUTION_CANVAS_SCALE = SOLUTION_CANVAS_SIZE / max
+  INSTANCE = getInstance(min, max, count)
+
+  const parsedPoints = parseCoordsData(INSTANCE)
+
+  drawPoints(SOLUTION_CANVAS_CONTEXT, parsedPoints[1])
 }
 
 document.querySelector('#solveGreedy').addEventListener('click', () => {
@@ -122,7 +150,7 @@ document.querySelector('#solveGreedy').addEventListener('click', () => {
 })
 
 document.querySelector('#generateInstance').addEventListener('click', () => {
-  generateInstance()
+  generateInstance(0, 1000, 100)
 })
 
 export default socket
