a3547a056dad799a24109c4eda28bb7489646caf
Hubert Józwowiak <hubert.jozwowiak@gmail.com>
2021-10-12 15:35:38 +0200

    [feat]: refactor - use Point struct and added function documentations

--- lib/tsp_solver/solver/helper.ex
+++ lib/tsp_solver/solver/helper.ex
@@ -1,5 +1,8 @@
 defmodule TspSolver.Solver.Helper do
-  @spec prepare_data_to_process(list()) :: {:ok, map()}
+  alias TspSolver.Solver.Point
+
+  @spec prepare_data_to_process(list()) ::
+          {:ok, %{starting_point: %Point{}, list_of_points: list(%Point{})}}
   def prepare_data_to_process([
         %{"index" => starting_point_index, "coords" => starting_point_coords}
         | list_of_points
@@ -9,9 +12,12 @@ defmodule TspSolver.Solver.Helper do
       coords: List.to_tuple(starting_point_coords)
     }
 
+    new_starting_point =
+      struct(Point, %{index: starting_point_index, coords: starting_point_coords})
+
     new_list_of_points =
       Enum.map(list_of_points, fn %{"index" => index, "coords" => coords} ->
-        %{index: index, coords: List.to_tuple(coords)}
+        struct(Point, %{index: index, coords: coords})
       end)
 
     {:ok, %{starting_point: new_starting_point, list_of_points: new_list_of_points}}
